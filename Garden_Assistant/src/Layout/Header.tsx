896a491a0308ca5c8d95f3272451006e8b5064ea
Ryan Guelzo <ryan.t.guelzo.ctr@health.mil>
2024-05-16 10:01:22 -0700

    Updated with User Info on Journal.

--- Garden_Assistant/src/Layout/Header.tsx
+++ Garden_Assistant/src/Layout/Header.tsx
@@ -1,7 +1,25 @@
+import { logout, useAuth } from "wasp/client/auth";
+import { Link } from "react-router-dom";
+
 const Header = () => {
+  const { data: user } = useAuth();
   return (
     <div className='bg-[#596720] h-12 flex items-center justify-center'>
       <h1 className='text-xl font-bold text-white'>Garden Assistant</h1>
+      <div className='absolute right-3'>
+        {user ? (
+          <button
+            onClick={logout}
+            className='border-2 border-white text-white p-2'
+          >
+            (Logout)
+          </button>
+        ) : (
+          <Link to='/login'>
+            <p className='text-xl2 underline text-white'>Log in</p>
+          </Link>
+        )}
+      </div>
     </div>
   );
 };
