fa336f18f206bc7a5863c5797d5a42bb9088e853
Ryan Guelzo <ryan.t.guelzo.ctr@health.mil>
2024-05-16 10:22:03 -0700

    Fixed Layout

--- Garden_Assistant/src/Layout/Layout.tsx
+++ Garden_Assistant/src/Layout/Layout.tsx
@@ -1,6 +1,6 @@
-import { ReactNode } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import { Link } from "react-router-dom";
 
 const Layout = ({ children }: any) => {
   return (
@@ -8,7 +8,16 @@ const Layout = ({ children }: any) => {
       <div className='absolute top-0 w-full'>
         <Header />
       </div>
-      <div className='flex justify-center items-center'>{children}</div>
+      <div className='flex flex-col mt-16'>
+        <div className='ml-5 p-o'>
+          <Link to='/' className='underline font-bold tezxt-lg'>
+            Home
+          </Link>
+        </div>
+        <div className='flex justify-center items-center -mt-48'>
+          {children}
+        </div>
+      </div>
       <div className='absolute bottom-0 w-full'>
         <Footer />
       </div>
